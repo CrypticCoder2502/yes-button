f49c203dd1c187cf983d1d678a21a3296d2464e8
ayush <ayusshhh232gmail.com>
2026-04-16 18:16:57 +0530

    new

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ yesBtn.addEventListener("click", () => {
   document.body.innerHTML = `
     <div class="success-screen">
       <h1>Yayyyy 🥰💖</h1>
-      <p>You are officially my Valentine 😍</p>
+      <p>We are officially going on a date, lets finalise a place and time as per you convenience 😍</p>
     </div>
   `;
 });
